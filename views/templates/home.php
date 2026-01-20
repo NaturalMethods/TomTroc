f3e92fb7c8dc27084a822cf3bb701137b1fb4ebf
NaturalMethods <nathansanchezpro@gmail.com>
2026-01-20 21:41:46 +0100

    Page d'acceuil du site 2 Ajout des sections: - Les derniers livres ajoutés - Comment ça marche - Nos valeurs

--- views/templates/home.php
+++ views/templates/home.php
@@ -2,14 +2,130 @@
 
 <div class="home">
 
-    <div class="joinus">
+    <section class="joinus">
         <div class="joinustxt">
             <h1 class="hus">Rejoignez nos lecteurs passionnés</h1>
             <p class="pus">Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres. </p>
-            <button class="discover">Découvrir</button>
+            <button class="discover greenButton">Découvrir</button>
         </div>
         <img class="joinusimg" src="./img/hamza-nouasria.png" alt="ImageJoinUs">
 
-    </div>
+    </section>
+
+    <section class="lastbooks">
+        <div class="lastbookstitle">
+            <h2>Les derniers livres ajoutés</h2>
+        </div>
+        <div class="newbookslists">
+            <div class="bookcard">
+
+                <img class="bookimg" src="./img/damiers.png" alt="Book 1">
+                <div class="bookcarddesc">
+                    <p>Titre du livre</p>
+                    <p>Auteur</p>
+                    <p>Vendu par : Pseudo</p>
+                </div>
+
+            </div>
+            <div class="bookcard">
+
+                <img class="bookimg" src="./img/damiers.png" alt="Book 1">
+                <div class="bookcarddesc">
+                    <p>Titre du livre</p>
+                    <p>Auteur</p>
+                    <p>Vendu par : Pseudo</p>
+                </div>
+
+            </div>
+            <div class="bookcard">
+
+                <img class="bookimg" src="./img/damiers.png" alt="Book 1">
+                <div class="bookcarddesc">
+                    <p>Titre du livre</p>
+                    <p>Auteur</p>
+                    <p>Vendu par : Pseudo</p>
+                </div>
+
+            </div>
+            <div class="bookcard">
+
+                <img class="bookimg" src="./img/damiers.png" alt="Book 1">
+                <div class="bookcarddesc">
+                    <p>Titre du livre</p>
+                    <p>Auteur</p>
+                    <p>Vendu par : Pseudo</p>
+                </div>
+
+            </div>
+
+        </div>
+        <button class="allBooks greenButton">Voir tous les livres</button>
+    </section>
+
+    <section class="howitworks">
+
+        <div class="howitworkstitle">
+            <h2>Comment ça marche?</h2>
+        </div>
+        <div class="howitworkstext">
+            <p>Échanger des livres avec TomTroc c’est simple et amusant ! Suivez ces étapes pour commencer :</p>
+        </div>
+        <div class="howitworkscards">
+
+            <div class="howitworkscard">
+                <p class="test">Inscrivez-vous gratuitement sur notre plateforme.</p>
+            </div>
+            <div class="howitworkscard">
+                <p>Ajoutez les livres que vous souhaitez échanger à votre profil.</p>
+            </div>
+            <div class="howitworkscard">
+                <p>Parcourez les livres disponibles chez d'autres membres.</p>
+            </div>
+            <div class="howitworkscard">
+                <p>Proposez un échange et discutez avec d'autres passionnés de lecture.</p>
+            </div>
+        </div>
+
+        <div>
+            <button class ="howitworksbutton borderGreenButton">Voir tous les livres</button>
+        </div>
+
+    </section>
+
+    <section class="ourbeliefs">
+
+        <div class="ourbeliefsimg">
+            <img src="./img/ourbeliefs.png">
+        </div>
+
+        <div class="ourbeliefstext">
+
+            <div class="ourbeliefstitle">
+                <h2>Nos valeurs</h2>
+            </div>
+
+            <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+            <p>  Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.</p>
+            <p>Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p></p>
+
+            <div class="ourbeliefssvg">
+
+                <div class="beliefsign">
+                    L'équipe Tom Troc
+
+
+                </div>
+
+                <svg width="122" height="104" viewBox="0 0 122 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 96.2239C2.29696 95.8231 6.2879 96.4857 7.64535 96.4799C34.2391 96.3671 77.2911 74.6938 96.4064 56.0077C109.127 40.7678 119.928 7.80676 85.8057 2.24498C65.0283 -1.14163 50.1873 26.798 62.0601 33.1479C66.0177 35.2646 78.258 25.6127 65.0283 12.4049C51.7986 -0.802991 39.7279 0.128338 35.3463 2.24498C15.417 7.74826 2.27208 42.7152 71.8127 87.7573C96.4064 103.687 121 102.997 121 102.997" stroke="#00AC66" stroke-width="2" stroke-linecap="round"/>
+                </svg>
+            </div>
+
+        </div>
+
+
+
+    </section>
 
 </div>
+
